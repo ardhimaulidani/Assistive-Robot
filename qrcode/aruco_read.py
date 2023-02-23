5b1bc33425b59d494b988b7d1716a97a7a1e7b95
ardhimaulidani <ardhi.maulidani@gmail.com>
2023-02-23 15:03:17 +0700

    First commit

---
+++ qrcode/aruco_read.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+# import the necessary packages
+from imutils.video import VideoStream
+import argparse
+import imutils
+import time
+import cv2
+import sys
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--type", type=str,
+	default="DICT_ARUCO_ORIGINAL",
+	help="type of ArUCo tag to detect")
+args = vars(ap.parse_args())
+
+# load the ArUCo dictionary and grab the ArUCo parameters
+print("[INFO] detecting '{}' tags...".format(args["type"]))
+
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_50)
+parameters =  cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 600 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=1000)
+
+	# detect ArUco markers in the input frame
+	(corners, ids, rejected) = detector.detectMarkers(frame)
+
+	# verify *at least* one ArUco marker was detected
+	if len(corners) > 0:
+		# flatten the ArUco IDs list
+		ids = ids.flatten()
+
+		# loop over the detected ArUCo corners
+		for (markerCorner, markerID) in zip(corners, ids):
+			# extract the marker corners (which are always returned
+			# in top-left, top-right, bottom-right, and bottom-left
+			# order)
+			corners = markerCorner.reshape((4, 2))
+			(topLeft, topRight, bottomRight, bottomLeft) = corners
+
+			# convert each of the (x, y)-coordinate pairs to integers
+			topRight = (int(topRight[0]), int(topRight[1]))
+			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+			topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+			# draw the bounding box of the ArUCo detection
+			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+			cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+			cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+			cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+
+			# compute and draw the center (x, y)-coordinates of the
+			# ArUco marker
+			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+			cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+
+			# draw the ArUco marker ID on the frame
+			cv2.putText(frame, str(markerID),
+				(topLeft[0], topLeft[1] - 15),
+				cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (0, 255, 0), 2)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
