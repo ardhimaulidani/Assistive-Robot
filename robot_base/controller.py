065886c4e44ed37e0efb10a3562a6b448cf74db3
Ardhika Maulidani <62376702+ardhimaulidani@users.noreply.github.com>
2023-07-05 20:42:37 +0700

    Update controller.py

--- robot_base/controller.py
+++ robot_base/controller.py
@@ -1,35 +1,40 @@
-# Import the pygame module
+# Import the PyGame Module
 import pygame
 from pygame.locals import *
 
-# Import hardware controller
+# Import Hardware Controller
 from move import moveDiff
 
-# Define constants for the screen width and height
+# Define Screen Width and Height
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 320
 
-# Initialize pygame
+# Initialize PyGame
 pygame.init()
 running = True
-# Create the screen object
+
+# Create the Screen Object
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 class joystick(object):
   def __init__(self):
+    # Init Joystick Object
     self.joycount = pygame.joystick.get_count()
     self.temp = []
     self.key = None
-    
+
+    # If Joystick is Not Detected
     if(self.joycount == 0):
       print("No joysticks were detected. Use keyboard control layout")
       self.temp.append(self.joystick_handler(-1))
+      
+    # Get Joystick ID
     else:
       for i in range(self.joycount):
           self.temp.append(self.joystick_handler(i))
 
   def joystick_handler(self, id):
-    # Insert dummy data for controller
+    # Insert Dummy Object If Controller Not Detected
     if(id == -1):
       self.axis = [0,0,0,0,0,0]
       self.ball = [0,0,0,0]
@@ -37,6 +42,7 @@ class joystick(object):
       self.hat = [0,0,0,0]
       return self
       
+    # Create Controller Object
     else:
       self.id = id
       self.joy = pygame.joystick.Joystick(id)
@@ -62,15 +68,13 @@ class joystick(object):
       self.hat = []
       for i in range(self.numhats):
           self.hat.append(self.joy.get_hat(i))
-      return self #for self chaining
+      return self # For Self-Chaining
 
   def get_control(self):
     self.isPressed = None
-    # for loop through the event queue
     for event in pygame.event.get():
       # Check for KEYDOWN event      
       self.key = pygame.key.get_pressed()
-      # If the Esc key is pressed, then exit the main loop
       if (event.type == pygame.KEYDOWN):
         # Check for pressed key
         if self.key[pygame.K_RIGHT]:
@@ -97,11 +101,12 @@ class joystick(object):
           self.key = "r"
           self.isPressed = True
 
-      # Check for QUIT event. If QUIT, then set running to false.
+      # Check for QUIT event
       elif event.type == QUIT:
         pygame.quit()
         exit()
 
+      # Get Event if Joystick is Detected
       if(self.joycount >= 1):
         if event.type == JOYAXISMOTION:
           self.temp[event.joy].axis[event.axis] = event.value
@@ -118,34 +123,7 @@ class joystick(object):
         elif event.type == JOYBUTTONDOWN:
           self.isPressed = True
           self.temp[event.joy].button[event.button] = 1
-
       else:
         pass
 
     return(self.key, self.isPressed, self.temp)
-    
-
-if __name__ == "__main__":
-  gamepad = joystick()
-  drive = moveDiff(17, 27, 13, 23, 24, 12)
-
-  while(running):
-    # for loop through the event queue
-    key, isPressed, joy = gamepad.get_control()
-    # Fill the screen with black
-    screen.fill((0, 0, 0))
-
-    x, y, pwm = drive.joystickToDiff(joy[0].axis[0], -joy[0].axis[1])
-    # drive.move(x,y,pwm)
-    # print(x,y)
-    # Update the display
-    pygame.display.flip()
-    
-    # button 
-    # [0] = cross
-    # [1] = circle
-    # [2] = griangle
-    # [3] = square
-    # [9] = start
-
-    # hat
